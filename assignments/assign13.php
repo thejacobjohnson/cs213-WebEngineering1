f5653df344b4eb5324fb7c6778d58e4f1e60c9ff
thejacobjohnson <jacob.w.johnson12@icloud.com>
2019-12-13 19:41:41 -0700

    Finished assign13

--- assignments/assign13.php
+++ assignments/assign13.php
@@ -1,58 +1,59 @@
 
 <?php
-    $getFile = $_POST['getFile'];
-    if($getFile){
+    $getfile = $_GET['getFile'];
+    if ($getfile){
         echo file_get_contents("../data/registration.txt");
     } else {
-    $perf = $_POST['performance'];
-    $loc = $_POST['location'];
-    $room= $_POST['room'];
-    $time = $_POST['time_slot'];
-    $fName= $_POST['first_name'];
-    $lName= $_POST['last_name'];
-    $sID= $_POST['student_id'];
-    $skill = $_POST['skill'];
-    $instrument = $_POST['instrument'];
-    $fName2 = $_POST['first_name_2'];
-    $lName2 = $_POST['last_name_2'];
-    $sID2 = $_POST['student_id_2'];
-    $skill2 = $_POST['skill_2'];
-    $instrument2 = $_POST['instrument_2'];
+        $perf = $_GET['performance'];
+        $loc = $_GET['location'];
+        $room = $_GET['room'];
+        $time = $_GET['time_slot'];
+        $fName = $_GET['first_name'];
+        $lName = $_GET['last_name'];
+        $sID = $_GET['student_id'];
+        $skill = $_GET['skill'];
+        $instrument = $_GET['instrument'];
+        $fName2 = $_GET['first_name2'];
+        $lName2 = $_GET['last_name2'];
+        $sID2 = $_GET['student_id2'];
+        $skill2 = $_GET['skill2'];
+        $instrument2 = $_GET['instrument2'];
 
-    $name1 = $fName ." ". $lName;
-    if ($fName2 != null) {
-        $name2 = $fName2 . " " . $lName2;
-        $name = $name1 . " and " . $name2;
-    } else {
-        $name = $name1;
-    }
-    $location = $loc . "\n". "Room " .$room;
-    if ($skill2 != null){
-        $skills = $skill ." and ". $skill2;
-    } else {
-        $skills = $skill;
-    }
-    $performance = $perf . " of ". $skills;
+        $name1 = $fName . " " . $lName;
+        if ($perf == "Duet") {
+            $name2 = $fName2 . " " . $lName2;
+            $name = $name1 . " and " . $name2;
+        } else {
+            $name = $name1;
+        }
+        $location = $loc . "\n" . "Room " . $room;
+        if ($perf == "Duet") {
+            $skills = $skill . " and " . $skill2;
+        } else {
+            $skills = $skill;
+        }
+        $performance = $perf . ", skill level " . $skills;
 
-    $performanceDetail = "<div class=\"flex-row\">
+        $performanceDetail = "<div class=\"flex-row\">
                 <div class=\"flex-item headers\">
-                    <p>".$name."</p>
+                    <p>" . $name . "</p>
                 </div>
                 <div class=\"flex-item headers\">
-                    <p>".$location."</p>
+                    <p>" . $location . "</p>
                 </div>
                 <div class=\"flex-item headers\">
-                    <p>".$time."</p>
+                    <p>" . $time . "</p>
                 </div>
                 <div class=\"flex-item headers\">
-                    <p>".$performance."</p>
+                    <p>" . $performance . "</p>
                 </div>
             </div>
             <hr>";
-
-    $myfile = fopen("../data/registration.txt", "a") or die("Unable to open file!");
-    fwrite($myfile, $performanceDetail);
-    fclose($myfile);
+        if ($name != null) {
+            $myfile = fopen("../data/registration.txt", "a") or die("Unable to open file!");
+            fwrite($myfile, $performanceDetail);
+            fclose($myfile);
+        }
+        echo file_get_contents("../data/registration.txt");
     }
-    echo file_get_contents("../data/registration.txt");
 ?>
